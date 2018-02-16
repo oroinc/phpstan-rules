6a8d2ddbd062c15fadea81e43b7a6abcd46b3f2b
Dmitry Khrysev <x86demon@users.noreply.github.com>
2018-02-16 15:37:36 +0200

    removed misadded file (#16775)

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CHECK_PATH=/var/www/projects/oro-mono/master
-rm -rf logs;
-mkdir logs;
-ls ${CHECK_PATH}/package \
-| grep -v "\-demo" | grep -v "demo-" | grep -v "test-" | grep -v "german-" \
-| parallel -j 4  "./vendor/bin/phpstan analyze -c config.neon ${CHECK_PATH}/package/{} --autoload-file=${CHECK_PATH}/application/commerce-crm-ee/app/autoload.php > logs/{}.log"
-cd logs
-grep -l "\[OK\] No errors" * | xargs rm
